b909787133dd8783ae778b5f89fae3ea4f1a4b3d
Jason Dempsey <jasondemps1@gmail.com>
2026-02-15 10:28:46 -0800

    fix for mac stuff

--- lisp/init-style.el
+++ lisp/init-style.el
@@ -3,7 +3,9 @@
  ((eq system-type 'windows-nt)
   (set-frame-font "BlexMono Nerd Font Mono SemiBol 12" nil t))
  ((eq system-type 'gnu/linux)
-  (set-frame-font "BlexMono Nerd Font Mono SemiBold 12" nil t)))
+  (set-frame-font "BlexMono Nerd Font Mono SemiBold 12" nil t))
+ ((eq system-type 'darwin)
+  (set-frame-font "BlexMono Nerd Font Mono SemiBold 16" nil t)))
 
 ;;(print (font-family-list))
 (use-package dracula-theme
