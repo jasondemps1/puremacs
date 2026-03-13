c66e04ddbc7533e538a5d92c3e26fd056846faed
Jason Dempsey <jason@offworld.dev>
2026-03-13 07:40:18 -0400

    updated web stuff

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -8,6 +8,9 @@
   ("\\.jsx?\\'" . web-mode)
   ("\\.tsx?\\'" . web-mode)
   ("\\.json\\'" . web-mode)
-  ("\\.vue\\'" . web-mode))
+  ("\\.vue\\'" . web-mode)
+  :init
+  (setq web-mode-engines-alist
+        '(("svelte" . "\\.svelte\\'"))))
 
 (provide 'init-web)
